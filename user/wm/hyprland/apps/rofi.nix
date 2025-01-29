56eec81b36fe94b4e28768dd810fa3454e8217e9
TheHyperZtar <thehyperztar@gmail.com>
2025-01-29 14:08:28 -0700

    using nix features ++

--- user/wm/hyprland/apps/rofi.nix
+++ user/wm/hyprland/apps/rofi.nix
@@ -40,9 +40,9 @@
         orientation = mkLiteral "vertical";
         cursor = mkLiteral "default";
         spacing = mkLiteral "0px";
-        border = mkLiteral "${config.wayland.windowManager.hyprland.config.general.border_size}px";
+        border = mkLiteral "${config.wayland.windowManager.hyprland.settings.general.border_size}px";
         border-color = "@border-color";
-        border-radius = mkLiteral "${config.wayland.windowManager.hyprland.config.decoration.rounding}px";
+        border-radius = mkLiteral "${config.wayland.windowManager.hyprland.settings.decoration.rounding}px";
         background-color = mkLiteral "@bg";
       };
       "mainbox" = {
