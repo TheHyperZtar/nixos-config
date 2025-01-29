dfd20fa722c2c21a1a38e69dc6920611929e6849
TheHyperZtar <thehyperztar@gmail.com>
2025-01-28 17:29:57 -0700

    dunst is better 2

--- user/wm/hyprland/apps/dunst.nix
+++ user/wm/hyprland/apps/dunst.nix
@@ -1,9 +1,10 @@
-{...}: {
+{pkgs, ...}: {
+  home.packages = [pkgs.libnotify];
   services.dunst = {
     enable = true;
     settings = {
       global = {
-        offset = "30x50";
+        offset = "(10, 50)";
         dmenu = "rofi -dmenu";
         corner_radius = 10;
       };
