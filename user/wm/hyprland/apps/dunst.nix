4f2b879a7e5ca1eb903b4190d66cca0344468c6d
TheHyperZtar <thehyperztar@gmail.com>
2025-01-28 18:03:17 -0700

    dunst is better final final ++

--- user/wm/hyprland/apps/dunst.nix
+++ user/wm/hyprland/apps/dunst.nix
@@ -1,13 +1,19 @@
-{pkgs, ...}: {
+{config, pkgs, ...}: {
   home.packages = [pkgs.libnotify];
   services.dunst = {
     enable = true;
+    iconTheme = {
+      name = config.stylix.iconTheme.dark;
+      package = config.stylix.iconTheme.package;
+      size = "64x64";
+    };
     settings = {
       global = {
         width = "(0, 500)";
         offset = "(20, 20)";
-        dmenu = "rofi -dmenu";
+        dmenu = "rofi -dmenu -p Dunst -config ~/.config/rofi/config-long.rasi";
         corner_radius = 10;
+        min_icon_size = 64;
       };
     };
   };
