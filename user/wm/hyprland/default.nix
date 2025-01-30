3e200eee05370c4bd410414733680f1182d82255
TheHyperZtar <thehyperztar@gmail.com>
2025-01-29 21:56:43 -0700

    testing animations 3

--- user/wm/hyprland/default.nix
+++ user/wm/hyprland/default.nix
@@ -91,22 +91,7 @@
         };
       };
       animations = {
-        enabled = true;
-        bezier = [
-          "wind, 0.05, 0.9, 0.1, 1.05"
-          "winIn, 0.1, 1.1, 0.1, 1.1"
-          "winOut, 0.3, -0.3, 0, 1"
-          "liner, 1, 1, 1, 1"
-        ];
-        animation = [
-          "windows, 1, 6, wind, slide"
-          "windowsIn, 1, 6, winIn, slide"
-          "windowsOut, 1, 5, winOut, slide"
-          "windowsMove, 1, 5, wind, slide"
-          "border, 1, 1, liner"
-          "fade, 1, 10, default"
-          "workspaces, 1, 5, wind"
-        ];
+        enabled = false;
       };
       misc = {
         disable_hyprland_logo = true;
