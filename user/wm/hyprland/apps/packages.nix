21751ccb22822d7bd40153516fd7c0dd7b4cebbf
TheHyperZtar <thehyperztar@gmail.com>
2025-01-31 12:02:14 -0700

    hyprland keybinds

--- user/wm/hyprland/apps/packages.nix
+++ user/wm/hyprland/apps/packages.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}: {
   services.blueman-applet.enable = true;
   home.packages = with pkgs; [
+    brightnessctl
     font-manager
     grim
+    hyprpicker
     nemo-with-extensions
     nemo-fileroller
     pavucontrol
--- user/wm/hyprland/default.nix
+++ user/wm/hyprland/default.nix
@@ -8,12 +8,10 @@
     ./apps/cliphist.nix
     ./apps/dunst.nix
     ./apps/hyprlock.nix
-    #./apps/mako.nix
     ./apps/waybar.nix
     ./apps/wlogout
     ./apps/rofi.nix
     ./apps/swappy.nix
-    #./apps/swaync.nix
     ./apps/packages.nix
   ];
   xdg = {
@@ -37,7 +35,6 @@
     enable = true;
     xwayland.enable = true;
     settings = {
-      "$mainMod" = "SUPER";
       env = [
         "NIXOS_XDG_OPEN_USE_PORTAL,1"
         "WLR_RENDERER_ALLOW_SOFTWARE,1"
@@ -120,73 +117,81 @@
         mfact = 0.5;
       };
       bind = [
-        "$mainMod, RETURN, exec, kitty"
-        "$mainMod, Q, killactive"
-        "$mainMod, F, fullscreen"
-        "$mainMod, E, exec, nemo"
-        "$mainMod, W, exec, firefox"
-        "$mainMod, R, exec, rofi -show drun -show-icons"
-        "$mainMod, D, exec, rofimoji --use-icons -a copy --max-recent 10 --skin-tone light --no-frecency"
-        "$mainMod SHIFT, F, fullscreen, 1"
-        "$mainMod, P, exec, wlogout"
+        "SUPER, RETURN, exec, kitty"
+        "SUPER, Q, killactive"
+        "SUPER, F, fullscreen"
+        "SUPER SHIFT, F, fullscreen, 1"
+        "SUPER, E, exec, nemo"
+        "SUPER, W, exec, firefox"
+        "SUPER, R, exec, rofi -show drun -show-icons"
+        "SUPER SHIFT, R, exec, rofimoji --use-icons -a copy --max-recent 10 --skin-tone light --no-frecency"
+        "SUPER SHIFT CTRL, R, exec, cliphist list | rofi -dmenu -no-show-icons -p Clipboard -config ~/.config/rofi/config-long.rasi| cliphist decode | wl-copy"
+        "SUPER, P, exec, wlogout"
+        ''SUPER, S, exec, grim -g "$(slurp -d)" - | ${pkgs.imagemagick}/bin/magick - -shave 1x1 PNG:- | wl-copy''
+        ''SUPER SHIFT, S, exec, grim -g "$(slurp -d)" - | ${pkgs.imagemagick}/bin/magick - -shave 1x1 PNG:- | swappy -f -''
+        "SUPER, D, exec, vesktop"
+        "SUPER, C, exec, hyprpicker -a"
+        "SUPER, T, togglefloating,"
+        "SUPER, left, movefocus, l"
+        "SUPER, right, movefocus, r"
+        "SUPER, up, movefocus, u"
+        "SUPER, down, movefocus, d"
+        "SUPER, H, movefocus, l"
+        "SUPER, L, movefocus, r"
+        "SUPER, K, movefocus, u"
+        "SUPER, J, movefocus, d"
+        "SUPER SHIFT, left, movewindow, l"
+        "SUPER SHIFT, right, movewindow, r"
+        "SUPER SHIFT, up, movewindow, u"
+        "SUPER SHIFT, down, movewindow, d"
+        "SUPER SHIFT, H, movewindow, l"
+        "SUPER SHIFT, L, movewindow, r"
+        "SUPER SHIFT, K, movewindow, u"
+        "SUPER SHIFT, J, movewindow, d"
+        "SUPER, 1, workspace, 1"
+        "SUPER, 2, workspace, 2"
+        "SUPER, 3, workspace, 3"
+        "SUPER, 4, workspace, 4"
+        "SUPER, 5, workspace, 5"
+        "SUPER, 6, workspace, 6"
+        "SUPER, 7, workspace, 7"
+        "SUPER, 8, workspace, 8"
+        "SUPER, 9, workspace, 9"
+        "SUPER, 0, workspace, 10"
+        "SUPER SHIFT, 1, movetoworkspace, 1"
+        "SUPER SHIFT, 2, movetoworkspace, 2"
+        "SUPER SHIFT, 3, movetoworkspace, 3"
+        "SUPER SHIFT, 4, movetoworkspace, 4"
+        "SUPER SHIFT, 5, movetoworkspace, 5"
+        "SUPER SHIFT, 6, movetoworkspace, 6"
+        "SUPER SHIFT, 7, movetoworkspace, 7"
+        "SUPER SHIFT, 8, movetoworkspace, 8"
+        "SUPER SHIFT, 9, movetoworkspace, 9"
+        "SUPER SHIFT, 0, movetoworkspace, 10"
+        "SUPER, mouse_down, workspace, e+1"
+        "SUPER, mouse_up, workspace, e-1"
+        "SUPER, Tab, workspace, e+1"
+        "SUPER SHIFT, Tab, workspace, e-1"
         ", XF86PowerOff, exec, wlogout"
-        ''$mainMod, S, exec, grim -g "$(slurp -d)" - | ${pkgs.imagemagick}/bin/magick - -shave 1x1 PNG:- | wl-copy''
-        '', PRINT, exec, grim -g "$(slurp -d)" - | ${pkgs.imagemagick}/bin/magick - -shave 1x1 PNG:- | swappy -f -''
-        ''$mainMod, PRINT, exec, grim -g "$(slurp -d)" - | ${pkgs.imagemagick}/bin/magick - -shave 1x1 PNG:- | wl-copy''
-        "$mainMod SHIFT, B, exec, pkill waybar;waybar"
-        "$mainMod SHIFT, D, exec, cliphist list | rofi -dmenu -no-show-icons -p Clipboard -config ~/.config/rofi/config-long.rasi| cliphist decode | wl-copy"
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
-        "$mainMod, H, movefocus, l"
-        "$mainMod, J, movefocus, d"
-        "$mainMod, K, movefocus, u"
-        "$mainMod, L, movefocus, r"
-        "$mainMod SHIFT, left, resizeactive, -100 0"
-        "$mainMod SHIFT, right, resizeactive, 100 0"
-        "$mainMod SHIFT, up, resizeactive, 0 -100"
-        "$mainMod SHIFT, down, resizeactive, 0 100"
-        "$mainMod SHIFT, H, resizeactive, -100 0"
-        "$mainMod SHIFT, J, resizeactive, 0 100"
-        "$mainMod SHIFT, K, resizeactive, 0 -100"
-        "$mainMod SHIFT, L, resizeactive, 100 0"
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-        "$mainMod, Tab, workspace, e+1"
-        "$mainMod SHIFT, Tab, workspace, e-1"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl -p spotify play-pause"
+        ", XF86AudioPlay, exec, playerctl -p spotify play-pause"
+        ", XF86AudioNext, exec, playerctl -p spotify next"
+        ", XF86AudioNext, exec, playerctl -p spotify next"
       ];
       binde = [
-        "$mainMod, KP_ADD, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
-        "$mainMod, KP_SUBTRACT, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
+        "SUPER, KP_ADD, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+        "SUPER, KP_SUBTRACT, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
+        "SUPER SHIFT, KP_ADD, exec, brightnessctl set +5%"
+        "SUPER SHIFT, KP_SUBTRACT, exec, brightnessctl set 5%-"
         ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
         ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
-        "$mainMod SHIFT, KP_ADD, exec, ${pkgs.brightnessctl}/bin/brightnessctl set +5%"
-        "$mainMod SHIFT, KP_SUBTRACT, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 5%-"
+        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ", XF86MonBrightnessDown, exec, brightnessctl set +5%"
       ];
       bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
       ];
     };
   };
