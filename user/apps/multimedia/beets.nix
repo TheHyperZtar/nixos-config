622d8dc700dacc0c8dc6704c5492843b52390c7c
TheHyperZtar <thehyperztar@gmail.com>
2025-02-20 17:33:44 -0700

    nomas

--- user/apps/multimedia/beets.nix
+++ user/apps/multimedia/beets.nix
@@ -1,6 +1,17 @@
 {...}: {
   programs.beets = {
     enable = true;
+    config = {
+      id3v23 = false;
+      replace = {
+        "/" = "∕";
+      };
+      plugins = ["fetchart"];
+      fetchart = {
+        sources = ["beetcamp coverart itunes amazon"];
+        high_resolution = true;
+      };
+    };
   };
 }
       
