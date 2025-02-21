b1dbfa3285cc3b3d48d1adaa802234a56ec59785
TheHyperZtar <thehyperztar@gmail.com>
2025-02-20 18:43:33 -0700

    aber

--- user/apps/multimedia/beets.nix
+++ user/apps/multimedia/beets.nix
@@ -7,12 +7,17 @@
       import = {
         copy = true;
         write = true;
+        unmatched = "~/Music/Miscellaneous";
       };
       paths = {
         default = "$albumartist/$album/$title";
       };
       replace = {
         "_" = "∕";
+      };
+      mbsync = {
+        metadata = ["artist" "album" "albumartist" "title" "track" "year"];
+        save_album_art = true;
       };
     };
   };
